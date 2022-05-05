74e93f6e37eb8c806b5b43ce7064be455a816067
stefanbucher <stefan.bucher@dialog.ch>
2022-05-05 14:39:25 +0200

    remove html.raw, add some basic input & output-validation

--- Source/Chinook.Web/Pages/CustomerSearch.cshtml.cs
+++ Source/Chinook.Web/Pages/CustomerSearch.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Security;
 using Chinook.Core.DataAccess;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -17,14 +18,26 @@ namespace Chinook.Web.Pages
 
         public void OnGet(string search)
         {
-            if (search == null)
+            if (!IsSearchStringValid(search))
                 return;
 
             var customer = _context.Customers.FirstOrDefault(x => x.LastName == search);
 
-            Result = customer != null
+            Result = EscapeStringOutput(customer != null
                 ? $"{customer.FirstName} {customer.LastName}"
-                : $"Search \"{search}\" did not find any customer.";
+                : $"Search \"{search}\" did not find any customer.");
+        }
+
+        private static bool IsSearchStringValid(string search)
+        {
+            // todo: add more input-validation and change it to model and use standard-validatory
+            return search != null && search.Length <= 20;
+        }
+
+        private static string EscapeStringOutput(string outputString)
+        {
+            // todo: add more output-validation
+            return SecurityElement.Escape(outputString);
         }
     }
 }
